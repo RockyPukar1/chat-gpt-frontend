148b7fe0f12efb14443b14187f12b9cc3056469f
Pukar Chhatkuli <22.student.pukar@broadwayinfosys.edu.np>
2023-03-17 19:16:59 +0545

    1:07:44

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,139 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+import send from "./assets/send.svg";
+import user from "./assets/user.png";
+import loadingIcon from "./assets/loader.svg";
+import bot from "./assets/bot.png";
+
+// let arr = [
+//   {type: "user", post: "fafafafa"},
+//   {type: "bot", post: "afafafa"},
+// ]
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    document.querySelector(".layout").scrollTop =
+      document.querySelector(".layout").scrollHeight;
+  }),
+    [posts];
+
+  const fetchBotResponse = async () => {
+    const { data } = await axios.post(
+      "http://localhost:4000",
+      { input },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return data;
+  };
+
+  const onSubmit = () => {
+    if (input.trim() === "") {
+      return;
+    }
+    updatePosts(input);
+    updatePosts("loading...", false, true);
+    setInput("");
+    fetchBotResponse().then((res) => {
+      console.log(res);
+      updatePosts(res.bot.trim(), true);
+    });
+  };
+
+  const autoTypingBotResponse = (text) => {
+    let index = 0;
+    let intervalId = setInterval(() => {
+      if (index < text.length) {
+        setPosts((prevState) => {
+          let lastItem = prevState.pop();
+          if (lastItem.type !== "bot") {
+            prevState.push({
+              type: "bot",
+              post: text.charAt(index - 1),
+            });
+          } else {
+            prevState.push({
+              type: "bot",
+              post: lastItem.post + text.charAt(index - 1),
+            });
+          }
+          return [...prevState];
+        });
+        index++;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 30);
+  };
+
+  const updatePosts = (post, isBot, isLoading) => {
+    if (isBot) {
+      autoTypingBotResponse(post);
+    } else {
+      setPosts((prev) => {
+        return [...prev, { type: isLoading ? "loading" : "user", post: post }];
+      });
+    }
+  };
+
+  const onKeyUp = () => {
+    if (event.key === "Enter" || event.which === 13) {
+      onSubmit();
+    }
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <main className="chatGPT-app">
+      <section className="chat-container">
+        <div className="layout">
+          {posts.map((post, index) => (
+            <div
+              key={index}
+              className={`chat-bubble ${
+                post.type === "bot" || post.type === "loading" ? "bot" : ""
+              }`}
+            >
+              <div className="avatar">
+                <img
+                  src={
+                    post.type === "bot" || post.type === "loading" ? bot : user
+                  }
+                  alt=""
+                />
+              </div>
+              {post.type === "loading" ? (
+                <div className="loader">
+                  <img src={loadingIcon} alt="" />
+                </div>
+              ) : (
+                <div className="post">{post.post}</div>
+              )}
+            </div>
+          ))}
+        </div>
+      </section>
+      <footer>
+        <input
+          value={input}
+          className="composebar"
+          type="text"
+          placeholder="Ask Anything"
+          onChange={(event) => setInput(event.target.value)}
+          onKeyUp={onKeyUp}
+          autoFocus
+        />
+        <div className="send-button" onClick={onSubmit}>
+          <img src={send} alt="" />
+        </div>
+      </footer>
+    </main>
+  );
 }
 
-export default App
+export default App;
